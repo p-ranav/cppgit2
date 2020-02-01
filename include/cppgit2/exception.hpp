6ac5a6bba2820d3145afeebe5797c01cc840932d
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-02-01 08:46:23 -0600

    Added exception class

---
+++ include/cppgit2/exception.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <exception>
+#include <git2.h>
+#include <string>
+
+namespace cppgit2 {
+
+  class exception : public std::exception {
+  public:
+    exception() {
+      auto error = git_error_last();
+      message_ = error ? error->message : "unknown error";
+    }
+    exception(const char * message) : message_(message) {}
+    exception(const std::string & message) : message_(message) {}
+    virtual ~exception() throw() {}
+    virtual const char * what() const throw() {
+      return message_.c_str();
+    }    
+
+  protected:
+    std::string message_;
+  };
+  
+}
