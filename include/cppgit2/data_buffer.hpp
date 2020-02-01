1fc5b3d143551a99a15b145d3eddd120e241ed38
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-02-01 14:19:24 -0600

    Added data buffer class w/ unit tests

---
+++ include/cppgit2/data_buffer.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#include <cppgit2/exception.hpp>
+#include <git2.h>
+#include <string>
+#include <cstdlib>
+#include <cstring>
+
+namespace cppgit2 {
+
+  class data_buffer {
+  public:
+    // Construct buffer of size n
+    // Contains a git_buf with char buffer of size n
+    data_buffer(size_t n);
+
+    // Construct buffer from libgit2 C ptr
+    data_buffer(const git_buf * c_ptr);
+
+    // Dispose internal buffer
+    ~data_buffer();
+
+    // Check quickly if buffer contains a NUL byte
+    bool contains_nul() const;
+
+    // Resize buffer allocation to make more space
+    // Currently, this will never shrink a buffer, only expand it    
+    void grow_to_size(size_t target_size);
+
+    // Check quickly if buffer looks like it contains binary data
+    bool is_binary() const;
+
+    // Set buffer to a copy of some raw data
+    void set_buffer(const std::string &buffer);
+
+    // Get string representation of data buffer
+    std::string to_string() const;
+
+    // Access libgit2 C ptr
+    const git_buf * c_ptr() const;
+
+  private:
+    git_buf c_struct_;    
+  };
+  
+}
