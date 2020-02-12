36b566fc87b9d9ba0f001e6c615aaea17aa98563
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-02-12 15:12:32 +0530

    Started adding samples

---
+++ samples/create_repository.cpp
@@ -0,0 +1,11 @@
+#include <cppgit2/repository.hpp>
+#include <iostream>
+using namespace cppgit2;
+
+int main(int argc, char **argv) {
+  if (argc == 2) {
+    repository repo(argv[1], false);
+  } else {
+    std::cout << "Usage: ./executable <repo_path>\n";
+  }
+}
