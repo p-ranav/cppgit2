13f5d6f46cae865d57aa080e90dcd2acd5d39780
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-03-09 20:19:20 -0500

    Minor updates

--- include/cppgit2/blob.hpp
+++ include/cppgit2/blob.hpp
@@ -43,59 +43,6 @@ public:
   // Access libgit2 C ptr
   const git_blob *c_ptr() const;
 
-  class filter : public libgit2_api {
-  public:
-
-    // Flag to control behavior of git_blob_filter
-    enum class flag {
-      // when set, filters will not be applied to binary files
-      check_for_binary = (1 << 0),
-      // When set, filters will not load configuration from the
-      // system-wide `gitattributes` in `/etc` (or system equivalent).
-      no_system_attributes = (1 << 1),
-      // When set, filters will be loaded from a `.gitattributes` file
-      // in the HEAD commit.
-      attributes_from_head = (1 << 2)
-    };
-
-    friend inline flag operator|(flag a, flag b) {
-      return static_cast<flag>(static_cast<uint32_t>(a) |
-        static_cast<uint32_t>(b));
-    }
-
-    friend inline flag operator&(flag a, flag b) {
-      return static_cast<flag>(static_cast<uint32_t>(a)&
-        static_cast<uint32_t>(b));
-    }
-
-    class options : public libgit2_api {
-    public:
-      options() {
-        c_ptr_ = &default_options_;
-      }
-
-      options(git_blob_filter_options* c_ptr) : c_ptr_(c_ptr) {}
-
-      int version() const { return c_ptr_->version; }
-      void set_version(int version) { c_ptr_->version = version; }
-
-      filter::flag flags() const { return static_cast<filter::flag>(c_ptr_->flags); }
-      void set_flags(filter::flag flags) {
-        c_ptr_->flags = static_cast<uint32_t>(flags);
-      }
-
-      git_blob_filter_options* c_ptr() { return c_ptr_; }
-
-    private:
-      friend class diff;
-      git_blob_filter_options* c_ptr_;
-      git_blob_filter_options default_options_;
-    };
-  };
-
-  // Get a buffer with the filtered content of a blob.
-  data_buffer filter(const std::string &as_path, filter::options options = filter::options());
-
 private:
   friend class repository;
   git_blob *c_ptr_;
