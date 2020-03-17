348f05ef8977d85444e37bbf7581ce82b7d595c5
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-03-16 19:25:46 -0500

    Minor updates to git_exception class

--- include/cppgit2/git_exception.hpp
+++ include/cppgit2/git_exception.hpp
@@ -12,14 +12,14 @@ public:
     message_ = error ? error->message : "unknown error";
   }
   git_exception(const char *message) : message_(message) {}
-  git_exception(const std::string &message) : message_(message) {}
+  git_exception(const std::string &message) : message_(message.c_str()) {}
   virtual ~git_exception() throw() {}
-  virtual const char *what() const throw() { return message_.c_str(); }
+  virtual const char *what() const throw() { return message_; }
 
   static void clear() { git_error_clear(); }
 
 protected:
-  std::string message_;
+  const char * message_;
 };
 
 } // namespace cppgit2
