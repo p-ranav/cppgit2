e93e0686f433d8063c9a7247554e1b3d09d148de
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-03-18 11:54:29 -0500

    Comments

--- samples/discover_path.cpp
+++ samples/discover_path.cpp
@@ -3,6 +3,9 @@ using namespace cppgit2;
 
 int main(int argc, char **argv) {
   if (argc == 2) {
+    // Discover repository path by walking up from argv[1]
+    // When a repository is discovered, its .git directory is returned
+    // as the result
     std::cout 
       << repository::discover_path(argv[1], true, "/")
       << std::endl;
