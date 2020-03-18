674733e70c24898b3168a40d13b493e2e86376f1
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-03-18 13:46:28 -0500

    Sample for repo.for_eacH_submodule

---
+++ samples/print_submodules.cpp
@@ -0,0 +1,17 @@
+#include <cppgit2/repository.hpp>
+#include <iostream>
+#include <string>
+using namespace cppgit2;
+
+int main(int argc, char **argv) {
+  if (argc == 2) {
+    auto repo = repository::open(argv[1]);
+
+    repo.for_each_submodule([](const submodule &module, const std::string &path){
+      std::cout << module.name() << " " << module.url() << std::endl;
+    });
+
+  } else {
+    std::cout << "Usage: ./executable <repo_path>\n";
+  }
+}
